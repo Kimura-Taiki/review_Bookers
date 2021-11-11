1e1b95172e6b1ef0cfb1dd5bf9e7d6455f4f4c85
“Kimura-Taiki” <taiguimucun850@gmail.com>
2021-11-11 09:30:15 +0000

    トップだけ修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # 新しい枝に来たぞ
-  root to: 'books#home'
+  root to: 'homes#top'
   resources :books
 end
