b900a1e8186a2be75eb07422fad220165216a27e
“Kimura-Taiki” <taiguimucun850@gmail.com>
2021-11-11 08:46:05 +0000

    新しい枝に到着したぞ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # get 'books/home'
-  # get 'books/index'
-  # get 'books/show'
-  # get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # 新しい枝に来たぞ
   root to: 'books#home'
   resources :books
 end
