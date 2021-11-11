05ae5b07bc6f28b83618c14a95773b36d598100b
“Kimura-Taiki” <taiguimucun850@gmail.com>
2021-11-11 14:44:00 +0000

    応用課題３の更地

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # 応用課題３の枝に到着したぞ
 
   # devise関連の道筋を定義します。
   devise_for :users
